2cf95e91d870a9c9c675cd1bd539d0a524e5c6e1
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-06-13 14:31:07 -0400

    mejor formato de objeto para el modulo de angular

--- web/assets/js/app/search.js
+++ web/assets/js/app/search.js
@@ -1,31 +1,35 @@
 'use strict';
 
-angular.module('DumbuDirectSearch', [ 'ngResource' ])
+(function(){
 
-.controller('MainController', [
+    var AppDumbu = angular.module('DumbuDirectSearch', [ 'ngResource' ]);
 
-'$scope', '$resource', '$log',
-function _MainController($scope, $resource, $log) {
-  
-  var igUsers = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('username'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: '/index.php/users/johndoe',
-    remote: {
-      url: '/index.php/users/%QUERY',
-      wildcard: '%QUERY'
-    }
-  });
+    AppDumbu.MainController = function MainController($scope, $resource, $log) {
+        
+        var igUsers = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('username'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            prefetch: '/index.php/users/johndoe',
+            remote: {
+                url: '/index.php/users/%QUERY',
+                wildcard: '%QUERY'
+            }
+        });
+        
+        $('#ref-prof').typeahead(null, {
+            name: 'ig-profs',
+            hint: true,
+            highlight: true,
+            display: 'username',
+            source: igUsers,
+            minLength: 3
+        });
+        
+    };
 
-  $('#ref-prof').typeahead(null, {
-    name: 'ig-profs',
-    hint: true,
-    highlight: true,
-    display: 'username',
-    source: igUsers,
-    minLength: 3
-  });
-
-}
-
-]);
+    AppDumbu.controller('MainController', [ 
+        '$scope', '$resource', '$log', 
+        AppDumbu.MainController 
+    ]);
+    
+})();
