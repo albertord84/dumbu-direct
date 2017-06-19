7f8270680d04cbefa48b4dbdbc12610539d12dd5
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-06-19 15:13:14 -0400

    efecto que indica que se puede volver atras

--- web/assets/js/app/compose.js
+++ web/assets/js/app/compose.js
@@ -10,6 +10,16 @@
 
         AppDumbu.mainCtrlScope = $scope;
 
+        $('#logo p i.fa').pulsate({
+            color: '#09f',
+            reach: 20,
+            speed: 1000,
+            pause: 0,
+            glow: true,
+            repeat: 3,
+            onHover: false
+        });
+
     };
 
     AppDumbu.controller('MainController', [ 
