130740eb30ecf83dcd6c14b555482af08fda98b0
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-06-27 12:10:32 -0400

    un nuevo helper para generar cadena aleatoria

---
+++ app/application/helpers/d_guid_helper.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists('d_guid') )
+{
+    function d_guid()
+    {
+        return 'd_' . strtolower( sprintf('%04X%04X%04X%04X%04X',
+            mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535),
+            mt_rand(16384, 20479), mt_rand(32768, 49151)) );
+    }
+}
