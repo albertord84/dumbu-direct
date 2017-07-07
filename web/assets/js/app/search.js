31ca5d464b56fff4e3831eeb18b6c3872319b3ff
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-07-07 14:55:35 -0400

    perfiles por defecto amigables al envio de mensajes

--- web/assets/js/app/search.js
+++ web/assets/js/app/search.js
@@ -2,86 +2,87 @@
 
 //(function(){
 
-    var AppDumbu = angular.module('DumbuDirectSearch', [
-        'ngResource', 'ngCookies'
-    ]);
+var AppDumbu = angular.module('DumbuDirectSearch', [
+    'ngResource', 'ngCookies'
+]);
 
-    AppDumbu.MainController = function _MainController($scope, $log, $service, $location)
-    {
+AppDumbu.MainController = function _MainController($scope, $log, $service, $location)
+{
 
-        AppDumbu.scope = $scope;
+    AppDumbu.scope = $scope;
 
-        $scope.selectedProfs = [{
-            byline:"1.0m followers",
-            fullName:"Gatorade",
-            pk:"19933454",
-            profPic:"http://ig-s-d-a.akamaihd.net/hphotos-ak-xpa1/t51.2885-19/s150x150/14294715_1775264632730839_1178654801_a.jpg",
-            username:"gatorade"
-        },{
-            byline:"142k followers",
-            fullName:"IBM",
-            pk:"589638973",
-            profPic:"http://ig-s-b-a.akamaihd.net/hphotos-ak-xpa1/t51.2885-19/11821783_1706498382903293_1516970730_a.jpg",
-            username:"ibm"
-        },{
-            byline:"325k followers",
-            fullName:"Microsoft Lumia",
-            pk:"256679330",
-            profPic:"http://ig-s-c-a.akamaihd.net/hphotos-ak-xpa1/t51.2885-19/11313558_413215158851162_83535000_a.jpg",
-            username:"microsoftlumia"
-        }];
+    // Quitar una vez terminada la etapa de desarrollo
+    $scope.selectedProfs = [{
+        "pk": "4239955376", 
+        "profPic": "https://ig-s-a-a.akamaihd.net/hphotos-ak-xwa1/t51.2885-19/s150x150/15251735_1768912590039156_5995529154123005952_a.jpg", 
+        "username": "dumbu.02", 
+        "fullName": "Dumbu", 
+        "byline": "1 mutual follower"
+    }, {
+        "pk": "4492293740", 
+        "profPic": "https://ig-s-d-a.akamaihd.net/hphotos-ak-xwa1/t51.2885-19/s150x150/16122797_1831629543759927_3831911332626563072_a.jpg", 
+        "username": "dumbu.08", 
+        "fullName": "Dumbu", 
+        "byline": "4255 followers"
+    }, {
+        "pk": "4542814483", 
+        "profPic": "https://ig-s-a-a.akamaihd.net/hphotos-ak-xwa1/t51.2885-19/s150x150/16123160_1795657230699712_1231154969558646784_a.jpg", 
+        "username": "dumbu.09", 
+        "fullName": "Dumbu", 
+        "byline": "8155 followers"
+    }];
 
-        $scope.removeSelectedProfile = function _removeSelectedProfile($event) {
-            $service.removeSelectedProfile($event, $scope);
-        };
-
-        $scope.composeDirect = function _composeDirect() {
-            $service.saveSelectedProfileIds($scope);
-            // Redirigir hacia la pagina de componer el direct message
-            var frm = $service.getRedirForm({
-                action: '/index.php/compose',
-                method: 'post',
-                inputs: {
-                    pks: $scope.pks
-                }
-            });
-            frm.submit();
-        };
-
-        $scope.logout = function _logout() {
-            $location.url('/index.php/logout');
-        };
-
-        var igUsers = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('username'),
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            prefetch: '/index.php/users/johndoe',
-            remote: {
-                url: '/index.php/users/%QUERY',
-                wildcard: '%QUERY'
-            }
-        });
-
-        $('#ref-prof').typeahead(null, {
-            name: 'ig-profs',
-            hint: true,
-            highlight: true,
-            display: 'username',
-            source: igUsers,
-            minLength: 3
-        });
-
-        $('#ref-prof').on({
-            'typeahead:selected': function(e, datum) {
-                $service.selectProfile($scope, datum);
-            }
-        });
-
+    $scope.removeSelectedProfile = function _removeSelectedProfile($event) {
+        $service.removeSelectedProfile($event, $scope);
     };
 
-    AppDumbu.controller('MainController', [
-        '$scope', '$log', 'SearchService', '$location',
-        AppDumbu.MainController
-    ]);
+    $scope.composeDirect = function _composeDirect() {
+        $service.saveSelectedProfileIds($scope);
+        // Redirigir hacia la pagina de componer el direct message
+        var frm = $service.getRedirForm({
+            action: '/index.php/compose',
+            method: 'post',
+            inputs: {
+                pks: $scope.pks
+            }
+        });
+        frm.submit();
+    };
+
+    $scope.logout = function _logout() {
+        $location.url('/index.php/logout');
+    };
+
+    var igUsers = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('username'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: '/index.php/users/johndoe',
+        remote: {
+            url: '/index.php/users/%QUERY',
+            wildcard: '%QUERY'
+        }
+    });
+
+    $('#ref-prof').typeahead(null, {
+        name: 'ig-profs',
+        hint: true,
+        highlight: true,
+        display: 'username',
+        source: igUsers,
+        minLength: 3
+    });
+
+    $('#ref-prof').on({
+        'typeahead:selected': function (e, datum) {
+            $service.selectProfile($scope, datum);
+        }
+    });
+
+};
+
+AppDumbu.controller('MainController', [
+    '$scope', '$log', 'SearchService', '$location',
+    AppDumbu.MainController
+]);
 
 //})();
