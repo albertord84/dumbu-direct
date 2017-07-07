8328b8ed3802175a1827bbbdd8bb4d08d05dfe55
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-07-07 14:58:00 -0400

    se agregan los mensajes a la cola enviados desde el cliente web

--- app/application/controllers/Directs.php
+++ app/application/controllers/Directs.php
@@ -7,16 +7,27 @@ class Directs extends MY_Controller {
     {
         $message = $this->input->post('message');
         
+        $pks = $this->session->userdata('pks');
+        
         $data['session'] = $this->session->userdata();
         $data['message'] = $message;
         
         $user_id = $this->session->userdata('user_id');
 
         if ($user_id != NULL) {
-            /**
-             * DQManager->add($user_id, $pks, $message);
-             */
-            $this->load->view('directs_dashboard', $data);
+
+            $this->load->library('directs/queue/manager');
+            $dqm = new Manager();
+            $added = $dqm->add($user_id, $this->getLocalDate(), $pks, $message);
+
+            if ($added) {
+                $last_direct = $dqm->get($user_id);
+                $data['last_direct'] = $last_direct;
+                $this->load->view('directs_dashboard', $data);
+            }
+            else {
+                show_error('Something wrong happened trying to enqueue the message', 500);
+            }
             
             return;
         }
