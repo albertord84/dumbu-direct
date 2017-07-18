517940a9dc24659c7cb734f70e065943a9dc826f
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-07-18 16:33:00 -0400

    seguidores de 09 y viendo si son privados o publicos

--- test/userFollowers.php
+++ test/userFollowers.php
@@ -1,32 +1,35 @@
 <?php
-$_creds = file_get_contents(__DIR__.'/../app/application/config/instagram_credentials');
+
+$_creds = file_get_contents(__DIR__ . '/../app/application/config/instagram_credentials');
 $creds = explode(':', $_creds);
 set_time_limit(0);
 date_default_timezone_set('UTC');
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 $username = $creds[0];
 $password = $creds[1];
-$recip='dumbu.08';
-$photoFileName=dirname(__FILE__).'/assets/img/photo.jpg';
+$recip = 'dumbu.09';
+$photoFileName = dirname(__FILE__) . '/assets/img/photo.jpg';
 $debug = false;
-$truncatedDebug = false;
+$truncatedDebug = true;
 $captionText = '';
-$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+$ig = new \InstagramAPI\Instagram(true, $truncatedDebug);
 try {
     $ig->setUser($username, $password);
     $ig->login();
 } catch (\Exception $e) {
-    echo 'Something went wrong trying to login: '.$e->getMessage()."\n";
+    echo 'Something went wrong trying to login: ' . $e->getMessage() . "\n";
     exit(0);
 }
 try {
-  $resp = $ig->getUserFollowers($ig->getUsernameId($recip));
-  $users = $resp->users;
-  for($i=0; $i<count($users); $i++) {
-      $user = $users[ $i ];
-      echo sprintf("profile: %s / username: %s / private: %s", $user->pk, 
-              $user->username, $user->is_private ? 'true' : 'false');
-  }
+    $pk = $ig->getUsernameId($recip);
+    $resp = $ig->getUserFollowers($pk);
+    $users = $resp->users;
+    for ($i = 0; $i < count($users); $i++) {
+        $user = $users[$i];
+        echo sprintf("%s ==> profile: %s / username: %s / private: %s" . PHP_EOL, 
+                $i, $user->pk, $user->username, 
+                $user->is_private ? 'true' : 'false');
+    }
 } catch (\Exception $e) {
-    echo 'Something went wrong trying to get recent activity: '.$e->getMessage()."\n";
+    echo 'Something went wrong trying to get recent activity: ' . $e->getMessage() . "\n";
 }
