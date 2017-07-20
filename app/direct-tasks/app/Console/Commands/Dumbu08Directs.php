ff8dce9bd797dcebd3b2f57cda69d8b5c33a4e47
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-07-19 20:18:03 -0400

    exploto por faltar una comilla

--- app/direct-tasks/app/Console/Commands/Dumbu08Directs.php
+++ app/direct-tasks/app/Console/Commands/Dumbu08Directs.php
@@ -112,7 +112,7 @@ class Dumbu08Directs extends Command
 
     private function getFirstTenFileNames()
     {
-        $cmd = sprintf('ls %s/*.json" | grep %s | head', 
+        $cmd = sprintf('ls %s/*.json | grep %s | head', 
                 __DIR__ . '/../../../../application/logs/directs/queue',
                 $this->pk);
         $cmd_output = shell_exec($cmd);
