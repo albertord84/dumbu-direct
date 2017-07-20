adfb207841c6456caa1a1be669f320a696c04fba
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-07-19 20:47:27 -0400

    dudas con el movimiento del mensaje fuera de la cola

--- app/direct-tasks/app/Console/Commands/Dumbu08Directs.php
+++ app/direct-tasks/app/Console/Commands/Dumbu08Directs.php
@@ -92,15 +92,14 @@ class Dumbu08Directs extends Command
                 $resp = $this->instagram->directMessage($fileObj->pks[0], $fileObj->message);
                 echo sprintf("%s - Enviado mensaje %s al perfil %s" . PHP_EOL,
                     date('r'), basename($fileName), $fileObj->pks[0]);
-                $this->popMessage($fileName);
-                sleep(5);
             }
             catch (Exception $e) {
                 echo sprintf("%s - Error al enviar el mensaje a %s: %s" . PHP_EOL,
                     date('r'), $fileObj->pks[0], $e->getMessage());
                 //$this->changeThrottle();
-                $this->popMessage($fileName);
             }
+            $this->popMessage($fileName);
+            sleep(5);
         }
     }
     
