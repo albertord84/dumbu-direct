825d53d5ea893672bd03be50d8f56b6cf4639d71
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-07-21 17:00:20 -0400

    eliminando codigo no muy optimo de una clase que ya no es necesaria

--- app/direct-tasks/app/Console/Kernel.php
+++ app/direct-tasks/app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        '\App\Console\Commands\SendDirects',
+        //'\App\Console\Commands\SendDirects',
         '\App\Console\Commands\Dumbu08Directs',
         '\App\Console\Commands\Dumbu09Directs'
     ];
