ae17f7858b7df1ebf082c3c808898dd5e76aaf80
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-07-21 18:03:03 -0400

    obteniendo inbox del usuario

--- test/userInbox.php
+++ test/userInbox.php
@@ -1,30 +1,32 @@
 <?php
-$_creds = file_get_contents(dirname(__FILE__).'/instagram_credentials');
+
+$_creds = file_get_contents( __DIR__ . '/../app/application/config/instagram_credentials' );
 $creds = explode(':', $_creds);
 set_time_limit(0);
-date_default_timezone_set('UTC');
-require __DIR__.'/../vendor/autoload.php';
-$username = $creds[0];
-$password = $creds[1];
-$recip='alberto_dreyes';
-$photoFileName=dirname(__FILE__).'/assets/img/photo.jpg';
+require __DIR__ . '/../vendor/autoload.php';
+$username = trim($creds[0]);
+$password = trim($creds[1]);
 $debug = false;
-$truncatedDebug = false;
-$captionText = '';
+$truncatedDebug = true;
 $ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
 try {
     $ig->setUser($username, $password);
     $ig->login();
 } catch (\Exception $e) {
-    echo 'Something went wrong trying to login: '.$e->getMessage()."\n";
+    echo 'Something went wrong trying to login: ' . $e->getMessage() . "\n";
     exit(0);
 }
 try {
-  echo "{ \"pending\": ";
-  echo json_encode($ig->getPendingInbox());
-  echo ", \"inbox\": ";
-  echo json_encode($ig->getV2Inbox());
-  echo "}";
+    $threads = $ig->getV2Inbox()->inbox->threads;
+    for ($i = 0; $i < count($threads); $i++) {
+        if ($i == 10) exit(0);
+        $item = $threads[ $i ]->items[0];
+        $inviter = $threads[ $i ]->inviter->username;
+        $timestamp = date('Y-m-d H:i:s', $item->timestamp / 1000000);
+        $text = $item->text;
+        echo sprintf("%s ==> %s - \"%s\" escribio: \"%s...\"" . PHP_EOL, 
+                intval($i) + 1, $timestamp, $inviter, substr($text, 0, 20));
+    }
 } catch (\Exception $e) {
-    echo 'Something went wrong trying to get recent activity: '.$e->getMessage()."\n";
+    echo 'Something went wrong trying to get recent activity: ' . $e->getMessage() . "\n";
 }
