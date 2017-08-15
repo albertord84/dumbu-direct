665fdee8fd52ba874adc13c791e8a24aed85f71d
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2017-08-15 15:38:56 -0400

    crear cola de archivos usando parametros de la linea de comandos

--- test/create_mass_directs.php
+++ test/create_mass_directs.php
@@ -1,20 +1,29 @@
 <?php
 
+$ref_prof = $argv[1];
+
 $output_dir = '/tmp';
 
 $control_pk = "3670825632"; // Mi perfil para ir sabiendo cada 50 msg si todo va bien
 
-//$uid = "4492293740"; // dumbu.08
-$uid = '4542814483'; // dumbu.09
-$input_file = __DIR__ . "/../tmp/following";
+$dumbu08 = "4492293740"; // dumbu.08
+$dumbu09 = '4542814483'; // dumbu.09
 
-$message = "Ganhe milhares de seguidores por mês, turbine seu perfil!" . 
+$ref_profs = array(
+    'dumbu08' => $dumbu08,
+    'dumbu09' => $dumbu09
+);
+
+
+$input_file = __DIR__ . "/../tmp/$ref_prof.followers.txt";
+
+$message = "Win thousands of followers per month, boost your profile!" . 
         PHP_EOL . PHP_EOL . 
-        "Teste grátis por 7 dias. Sem multa de rescisão." . 
+        "50% discount the first month (use the promotional code INSTA50P)" . 
         PHP_EOL . PHP_EOL . 
-        "Use o código promocional INSTA-DIRECT. Esta promoção é valida apenas essa semana!!" . 
+        "This promotion is valid only this week!" . 
         PHP_EOL . PHP_EOL . 
-        "Acesse http://instadumbu.com";
+        "Go to http://instadumbu.com";
 
 // {"datetime":"20170710_150238","uid":"3670825632","pks":["4239955376"],"message":""}
 
@@ -31,10 +40,10 @@ if ($handle) {
         );
         $fn = date("Ymd");
         $data['datetime'] = sprintf("%s_%s", $fn, $datetime);
-        $data['uid'] = $uid;
+        $data['uid'] = $ref_profs[ $ref_prof ];
         $data['pks'][] = trim($line);
         $data['message'] = $message;
-        $output_file_name = sprintf("%s_%s_%s.json", $fn, $datetime, $uid);
+        $output_file_name = sprintf("%s_%s_%s.json", $fn, $datetime, $ref_profs[ $ref_prof ]);
         file_put_contents($output_dir . '/' . $output_file_name, json_encode($data) . PHP_EOL);
         echo sprintf("Creado mensaje %s/%s" . PHP_EOL, $output_dir, $output_file_name);
         $datetime++;
@@ -47,10 +56,10 @@ if ($handle) {
                 "message" => NULL
             );
             $data['datetime'] = sprintf("%s_%s", $fn, $datetime);
-            $data['uid'] = $uid;
+            $data['uid'] = $ref_profs[ $ref_prof ];
             $data['pks'][] = $control_pk;
             $data['message'] = sprintf("Mensaje de control. Enviados hasta aqui cerca de %s.", $c);
-            $output_file_name = sprintf("%s_%s_%s_chk.json", $fn, $datetime, $uid);
+            $output_file_name = sprintf("%s_%s_%s_chk.json", $fn, $datetime, $ref_profs[ $ref_prof ]);
             file_put_contents($output_dir . '/' . $output_file_name, json_encode($data) . PHP_EOL);
             echo sprintf("Creado mensaje %s/%s" . PHP_EOL, $output_dir, $output_file_name);
             $datetime++;
