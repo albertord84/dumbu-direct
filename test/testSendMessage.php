384b35374ea9445f4423216cef6b74cd5bf5ab56
Yordano Pascual <yordanoweb@yahoo.es>
2017-09-14 16:47:03 -0400

    nueva prueba de envio aislada

---
+++ test/testSendMessage.php
@@ -0,0 +1,24 @@
+<?php
+set_time_limit(0);
+date_default_timezone_set('UTC');
+require __DIR__.'/../vendor/autoload.php';
+$username = $argv[1];
+$password = $argv[2];
+$recip = $argv[3]; // nombre del perfil, no el id del perfil
+$msg = $argv[4];
+$debug = true;
+$truncatedDebug = false;
+$captionText = '';
+$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+try {
+    $ig->setUser($username, $password);
+    $ig->login();
+} catch (\Exception $e) {
+    echo 'Something went wrong trying to login: '.$e->getMessage()."\n";
+    exit(0);
+}
+try {
+    $ig->directMessage($recip, $msg);
+} catch (\Exception $e) {
+    echo 'Something went wrong trying to text to '.$recip.': '.$e->getMessage()."\n";
+}
