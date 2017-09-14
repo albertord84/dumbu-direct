b3b4a06a4d970878a272504cb156643aa014d3ce
Yordano Pascual <yordanoweb@yahoo.es>
2017-09-14 11:30:42 -0400

    nuevas tareas de carmenvecchio y wavcreators

--- app/direct-tasks/app/Console/Commands/CarmenVecchioDirects.php
+++ app/direct-tasks/app/Console/Commands/CarmenVecchioDirects.php
@@ -4,24 +4,21 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class PedroPettiDirects extends DirectsCommand
+class CarmenVecchioDirects extends DirectsCommand
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'sendirects:pedropetti';
+    protected $signature = 'sendirects:carmenvecchio';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Sends Instagram direct messages to pedropetti followers';
-    
-    // 5787797919 = pedropetti
-    // 236116119 = pbpetti
+    protected $description = 'Sends Instagram direct messages to carmenvecchio followers';
     
     /**
      * Create a new command instance.
@@ -31,16 +28,28 @@ class PedroPettiDirects extends DirectsCommand
     public function __construct()
     {
         parent::__construct();
-        $this->pk = '236116119';
-        $this->username = 'pbpetti';
-        $this->password = 'Pp75005310';
-        $this->suspended = TRUE;
+        $this->pk = '55324697';
+        $this->username = 'carmenvecchio';
+        $this->password = 'filippo1988';
+        //$this->suspended = TRUE;
         // 191.252.110.140:23128
         // 191.252.103.137:23128
         // 191.252.111.93:23128
         // 191.240.149.250:65103
         // 191.189.66.154:53281
-        $this->proxy = '191.252.111.93:23128';
+        $min = $this->currentMinute();
+        if ($min < 15) {
+            $this->proxy = '191.252.111.93:23128';
+        }
+        if ($min > 15 && $min < 30) {
+            $this->proxy = '191.252.110.140:23128';
+        }
+        if ($min > 30 && $min < 45) {
+            $this->proxy = '191.252.103.137:23128';
+        }
+        if ($min > 45 && $min < 59) {
+            $this->proxy = '201.90.120.197:3128';
+        }
     }
 
 }
