48e4847008b2061c77eb1121bc5e5d385ee5b057
Yordano Pascual <yordanoweb@yahoo.es>
2017-09-14 11:47:13 -0400

    nuevos proxies para probar nueva estrategia

--- app/direct-tasks/app/Console/Commands/PedroPettiDirects.php
+++ app/direct-tasks/app/Console/Commands/PedroPettiDirects.php
@@ -35,11 +35,6 @@ class PedroPettiDirects extends DirectsCommand
         $this->username = 'pbpetti';
         $this->password = 'Pp75005310';
         $this->suspended = TRUE;
-        // 191.252.110.140:23128
-        // 191.252.103.137:23128
-        // 191.252.111.93:23128
-        // 191.240.149.250:65103
-        // 191.189.66.154:53281
         $this->proxy = '191.252.111.93:23128';
     }
 
--- app/direct-tasks/app/Console/Commands/WavCreatorsDirects.php
+++ app/direct-tasks/app/Console/Commands/WavCreatorsDirects.php
@@ -32,20 +32,15 @@ class WavCreatorsDirects extends DirectsCommand
         $this->username = 'wavcreators';
         $this->password = 'theoath';
         //$this->suspended = TRUE;
-        // 191.252.110.140:23128
-        // 191.252.103.137:23128
-        // 191.252.111.93:23128
-        // 191.240.149.250:65103
-        // 191.189.66.154:53281
         $min = $this->currentMinute();
         if ($min < 15) {
-            $this->proxy = '191.252.111.93:23128';
+            $this->proxy = '191.252.103.106:23128';
         }
         if ($min > 15 && $min < 30) {
-            $this->proxy = '191.252.110.140:23128';
+            $this->proxy = '191.252.100.122:23128';
         }
         if ($min > 30 && $min < 45) {
-            $this->proxy = '191.252.103.137:23128';
+            $this->proxy = '191.252.109.233:23128';
         }
         if ($min > 45 && $min < 59) {
             $this->proxy = '201.90.120.197:3128';
