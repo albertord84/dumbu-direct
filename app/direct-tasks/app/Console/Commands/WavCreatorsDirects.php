0b45dec9af826064c7180b03dad8e11ce1f6c74b
Yordano Pascual <yordanoweb@yahoo.es>
2017-09-14 11:26:42 -0400

    para probar proxies

---
+++ app/direct-tasks/app/Console/Commands/WavCreatorsDirects.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PedroPettiDirects extends DirectsCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sendirects:pedropetti';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends Instagram direct messages to pedropetti followers';
+    
+    // 5787797919 = pedropetti
+    // 236116119 = pbpetti
+    
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->pk = '236116119';
+        $this->username = 'pbpetti';
+        $this->password = 'Pp75005310';
+        $this->suspended = TRUE;
+        // 191.252.110.140:23128
+        // 191.252.103.137:23128
+        // 191.252.111.93:23128
+        // 191.240.149.250:65103
+        // 191.189.66.154:53281
+        $this->proxy = '191.252.111.93:23128';
+    }
+
+}
