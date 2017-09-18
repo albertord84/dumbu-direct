932bcdb2fee67cfb52d140e0c79cef2025b0079a
Yordano Pascual <yordanoweb@yahoo.es>
2017-09-18 08:05:25 -0400

    prueba de obtener comentarios de un media

---
+++ test/testGetComments.php
@@ -0,0 +1,24 @@
+<?php
+set_time_limit(0);
+date_default_timezone_set('UTC');
+require __DIR__.'/../vendor/autoload.php';
+$username = $argv[1];
+$password = $argv[2];
+$media_id = $argv[3]; // id del medio
+$debug = true;
+$truncatedDebug = true;
+
+$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+try {
+    $ig->setUser($username, $password);
+    $ig->login();
+} catch (\Exception $e) {
+    echo 'Something went wrong trying to login: '.$e->getMessage()."\n";
+    exit(0);
+}
+try {
+    $ig->request("/media/$media_id/comments")
+       ->addParam('method', 'GET');
+} catch (\Exception $e) {
+    echo 'Something went wrong trying get comments'.$uid.': '.$e->getMessage()."\n";
+}
