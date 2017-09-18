718ab287e030036331f0bdae7a0db7ea70d1f1a1
Yordano Pascual <yordanoweb@yahoo.es>
2017-09-18 17:23:59 -0400

    incluyendo dependencia laravel desde directorio menos obsceno

---
+++ scheduler/app/Console/Commands/CarmenVecchioDirects.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CarmenVecchioDirects extends DirectsCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sendirects:carmenvecchio';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends Instagram direct messages to carmenvecchio followers';
+    
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->pk = '55324697';
+        $this->username = 'carmenvecchio';
+        $this->password = 'filippo1988';
+        $this->suspended = TRUE;
+        $proxyNumber = mt_rand(0,5);
+        $this->setProxyNumber($proxyNumber);
+    }
+
+}
---
+++ scheduler/app/Console/Commands/WavCreatorsDirects.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class WavCreatorsDirects extends DirectsCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sendirects:wavcreators';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends Instagram direct messages to wavcreators followers';
+    
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->pk = '246817495';
+        $this->username = 'wavcreators';
+        $this->password = 'theoath';
+        $this->suspended = TRUE;
+        $proxyNumber = mt_rand(0,5);
+        $this->setProxyNumber($proxyNumber);
+    }
+
+}
