b919cfe2f242275284e668f93d120de0547422c3
Yordano Pascual <yordanoweb@yahoo.es>
2017-11-27 11:57:22 -0500

    mostrar parte del buzon

--- test/testUserInbox.php
+++ test/testUserInbox.php
@@ -1,11 +1,29 @@
 <?php
 set_time_limit(0);
 require __DIR__ . '/../vendor/autoload.php';
+
 $username = $argv[1];
 $password = $argv[2];
 $debug = false;
 $truncatedDebug = true;
 $ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+
+function show_threads($threads) {
+  for ($i = 0; $i < count($threads); $i++) {
+    $item = $threads[ $i ]->items[0];
+    $inviter = $threads[ $i ]->inviter->username;
+    $pk = $threads[ $i ]->inviter->pk;
+    $timestamp = date('Y-m-d H:i:s', $item->timestamp / 1000000);
+    $text = $item->text;
+    $recips = [];
+    foreach ($threads[ $i ]->users as $recip) {
+          $recips[] = $recip->pk;
+    }
+    echo sprintf("%s ==> %s - \"%s(%s)\" escribio: \"%s...\" a [%s]" . PHP_EOL,
+      intval($i) + 1, $timestamp, $inviter, $pk, substr($text, 0, 20),
+      implode(',', $recips));
+  }
+}
 try {
     $ig->setUser($username, $password);
     $ig->login();
@@ -14,21 +32,13 @@ try {
     exit(0);
 }
 try {
-    $threads = $ig->getV2Inbox()->inbox->threads;
-    for ($i = 0; $i < count($threads); $i++) {
-        //if ($i == 100) exit(0);
-        $item = $threads[ $i ]->items[0];
-        $inviter = $threads[ $i ]->inviter->username;
-        $pk = $threads[ $i ]->inviter->pk;
-        $timestamp = date('Y-m-d H:i:s', $item->timestamp / 1000000);
-        $text = $item->text;
-        $recips = [];
-        foreach ($threads[ $i ]->users as $recip) {
-            $recips[] = $recip->pk;
-        }
-        echo sprintf("%s ==> %s - \"%s(%s)\" escribio: \"%s...\" a [%s]" . PHP_EOL,
-                intval($i) + 1, $timestamp, $inviter, $pk, substr($text, 0, 20),
-                implode(',', $recips));
+    $cursor = null;
+    for ($i = 0; $i < 5; $i++) {
+      $inbox = $ig->getV2Inbox($cursor)->inbox;
+      $cursor = $inbox->oldest_cursor;
+      $threads = $inbox->threads;
+      show_threads($threads);
+      sleep(mt_rand(3,8));
     }
 } catch (\Exception $e) {
     echo 'Something went wrong trying to get recent activity: ' . $e->getMessage() . "\n";
