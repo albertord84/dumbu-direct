0f8ba9d9f665523e84c82bee1d1764b1f056b4f4
Yordano Pascual Rivera <yordanoweb@yahoo.es>
2018-01-12 14:04:36 -0500

    probando auth con verificacion

---
+++ test/testTwoFactorLogin.php
@@ -0,0 +1,24 @@
+<?php
+set_time_limit(0);
+date_default_timezone_set('UTC');
+require __DIR__.'/../vendor/autoload.php';
+/////// CONFIG ///////
+$username = $argv[1];
+$password = $argv[2];
+$debug = true;
+$truncatedDebug = false;
+//////////////////////
+$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+try {
+	$loginResponse = $ig->login($username, $password);
+	if ($loginResponse !== null && $loginResponse->isTwoFactorRequired()) {
+		$twoFactorIdentifier = $loginResponse->getTwoFactorInfo()->getTwoFactorIdentifier();
+		// The "STDIN" lets you paste the code via terminal for testing.
+		// You should replace this line with the logic you want.
+		// The verification code will be sent by Instagram via SMS.
+		$verificationCode = trim(fgets(STDIN));
+		$ig->finishTwoFactorLogin($username, $password, $twoFactorIdentifier, $verificationCode);
+	}
+} catch (\Exception $e) {
+	printf("Something went wrong: %s\n", $e->getMessage());
+}
