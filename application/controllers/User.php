820e834437ac6acba1b07e2a5b34d5f78ad640ad
Yordano Pascual <yordanoweb@yahoo.es>
2018-04-11 15:50:46 -0400

    login solo contra la bd mysql

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -52,31 +52,32 @@ class User extends CI_Controller {
         $username = $this->input->post('username');
         $password = $this->input->post('password');
         
-        $this->clean_previous_instagram_session($username);
-        sleep(5);
-        
-        //$instagram = new \InstagramAPI\Instagram(FALSE, TRUE);
-        
         try {
             set_time_limit(0);
             $is_registered = $this->user_exists($username);
-            $pk = $this->instag_id($username);
+            $pk = NULL;
             if (!$is_registered) {
+                $pk = $this->instag_id($username);
                 $this->db->insert('client', [
                     'username' => $username,
                     'password' => $password,
                     'pk' => $pk
                 ]);
             }
-            //$instagram->login($username, $password, false, 18000);
-            //$this->session->pk = $instagram->account_id;
+            else {
+                $user = $this->user_data($username);
+                if ($user->password !== $password) {
+                    throw new Exception("You have the wrong username/password " .
+                        "combination. Try again...", 1);
+                }
+            }
             $this->session->pk = $pk;
             $this->session->username = $username;
             $is_admin = $this->is_admin($username);
 			$this->session->is_admin = $is_admin;
             $response = [
                 'success' => TRUE,
-                'pk' => $instagram->account_id,
+                'pk' => $pk,
                 'username' => $username,
                 'priv' => $is_admin ? 1 : 0
             ];
@@ -84,7 +85,7 @@ class User extends CI_Controller {
             $response = [ 'success' => FALSE, 'message' => $e->getMessage() ];
         }
         
-        $this->output->set_content_type('application/json')
+        return $this->output->set_content_type('application/json')
             ->set_status_header($response['success'] ? 200 : 500)
             ->set_output(json_encode($response));
     }
@@ -99,25 +100,21 @@ class User extends CI_Controller {
 		return $is_admin;
 	}
 
-	private function user_exists($username) {
+    private function user_exists($username) {
+        $this->load->database();
+        $this->db->where('username', $username);
+        $query = $this->db->get('client');
+        return $query->num_rows() == 0 ? FALSE : TRUE;
+    }
+
+	private function user_data($username) {
 		$this->load->database();
 		$this->db->where('username', $username);
-		$query = $this->db->get('client');
-		return $query->num_rows() == 0 ? FALSE : TRUE;
-	}
-
-	private function clean_previous_instagram_session($username)
-	{
-		$dir = INSTAGRAM_SESSIONS . '/' . $username;
-		if (file_exists($dir)) {
-			shell_exec("rm -r " . $dir);
-		}
+		$users = $this->db->get('client')->result();
+		return $users[0];
 	}
 
 	public function logout() {
-		if ($this->session->username !== NULL) {
-			$this->clean_previous_instagram_session($this->session->username);
-		}
 		session_destroy();
 		$this->session->set_userdata([]);
 		$this->load->view('login_form');
